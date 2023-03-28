f0c63c9969f09e7f06b1bf77f160667dd04fd985
Peter Disney <peter.w.disney@gmail.com>
2023-03-28 13:42:52 -0700

    added domain address to admin controller

--- server/controllers/Admin.controller.js
+++ server/controllers/Admin.controller.js
@@ -69,7 +69,7 @@ module.exports.adminLogin = async (req, res) => {
     const adminToken = jwt.sign(payload, process.env.ADMIN_LOGIN_REG_SECRET_KEY)
 
     res
-      .cookie("adminToken", adminToken, { httpOnly: true, sameSite: 'strict', secure: true })
+      .cookie("adminToken", adminToken, { domain: "100.25.180.240" })
       .json({ msg: "login succesful" })
   } catch (err) {
     console.log(err);
