0dbb955ad3ba3e4cb0ffdc52444c7f6101d6f767
Peter Disney <peter.w.disney@gmail.com>
2023-03-29 16:58:23 -0700

    updated order routes search page

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -5,7 +5,7 @@ const { check } = require('express-validator');
 module.exports = app => {
     app.get('/api/order/view/:page', OrderController.getAllOrders);
     app.get('/api/order/:id', check('id').isMongoId(), OrderController.findoneSingleOrder);
-    app.get('/api/search/:searchTerm/:page', OrderController.searchOrders);
+    app.get('/api/order/search/:searchTerm/:page', OrderController.searchOrders);
     app.post('/api/order', OrderController.createNewOrder);
     app.put('/api/order/:id', OrderController.updateExistingOrder);
     app.delete('/api/order/:id', OrderController.deleteAnExistingOrder);
