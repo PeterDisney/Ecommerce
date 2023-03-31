cda67a158401d9a3db644a6974f21ae74dc67c64
Peter Disney <peter.w.disney@gmail.com>
2023-03-31 10:49:36 -0700

    admin controller single comment fix

--- server/controllers/Admin.controller.js
+++ server/controllers/Admin.controller.js
@@ -69,6 +69,7 @@ module.exports.adminLogin = async (req, res) => {
     const adminToken = jwt.sign(payload, process.env.ADMIN_LOGIN_REG_SECRET_KEY)
 
     res
+      // cookie response for the admin token and domain name
       .cookie("adminToken", adminToken, { domain: ".motomarket.shop" })
       .json({ msg: "login succesful" })
   } catch (err) {
